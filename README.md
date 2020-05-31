7423d45871121699c7a7b1451e9603613cc56484
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-05-31 17:54:02 -0300

    feature: Generate localized pages

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Experimenting Eleventy for Static Site Generation
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/9cb329b2-240a-44ad-9e5e-9713710bb096/deploy-status)](https://app.netlify.com/sites/eleventy-demo-jamstack/deploys)
+
+## JAMstack 🍯🔥
+
+JAMstack is a modern architecture to create **fast** and **secure sites** and dynamic apps delivered by **pre-rendering files** and serving them directly from a **CDN**.
--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,26 @@
-require('dotenv').config();
-const fetch = require('node-fetch');
+require("dotenv").config();
+const fetch = require("node-fetch");
+const countries = require("./countries.json");
 
-module.exports = async function getNewsArticles() {
+async function getNewsByCountry(country) {
   try {
     const response = await fetch(
-      `http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`
+      `http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=5`
     );
     const data = await response.json();
-    return data;
+    return {
+      country: country,
+      articles: data.articles,
+    };
   } catch (error) {
     console.error(error);
   }
+}
+
+module.exports = async function () {
+  const newsProms = countries.map(getNewsByCountry);
+  return Promise.all(newsProms).then((newsObj) => {
+    console.log(newsObj);
+    return [...newsObj];
+  });
 };
